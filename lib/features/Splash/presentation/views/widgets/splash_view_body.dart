850ae65a544c17aba6b6fff8268bfac5e58fb5d0
Khaled Khalifa <96270919+khaledkhalifa74@users.noreply.github.com>
2024-02-12 09:50:17 +0200

    create splash view

---
+++ lib/features/Splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,74 @@
+import 'package:bookly/constants.dart';
+import 'package:bookly/features/Home/presentation/views/home_view.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    initSlidingAnimation();
+    navigateToHome();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: Image.asset(
+            'assets/images/bookly_logo.png',
+            width: 300,
+            height: 300,
+          ),
+        ),
+        AnimatedBuilder(
+          animation: slidingAnimation,
+          builder: (context, _) {
+            return SlideTransition(
+              position: slidingAnimation,
+              child: const Text(
+                'Read Free Books',
+                style: TextStyle(
+                  fontSize: 24,
+                ),
+              ),
+            );
+          },
+        ),
+      ],
+    );
+  }
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Get.to(() => const HomeView(),
+          transition: Transition.fade,
+          duration: kTransitionDuration,
+      );
+    });
+  }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 5), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:bookly/constants.dart';
+import 'package:bookly/features/Splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+      ),
+      home: const SplashView(),
+    );
+  }
+}
