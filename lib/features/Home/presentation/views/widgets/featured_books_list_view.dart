a606c17683c38f5befa135b676e6994659c2935a
Khaled Khalifa <96270919+khaledkhalifa74@users.noreply.github.com>
2024-02-14 19:03:13 +0200

    add feature list view

---
+++ lib/features/Home/presentation/views/widgets/featured_books_list_view.dart
@@ -0,0 +1,24 @@
+import 'package:bookly/features/Home/presentation/views/widgets/featured_list_view_item.dart';
+import 'package:flutter/material.dart';
+
+class FeaturedBooksListView extends StatelessWidget {
+  const FeaturedBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.3,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index){
+            return const Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 8,
+              ),
+              child: FeaturedListViewItem(),
+            );
+          },
+      ),
+    );
+  }
+}
---
+++ lib/features/Home/presentation/views/widgets/featured_list_view_item.dart
@@ -0,0 +1,23 @@
+import 'package:bookly/core/widgets/assets.dart';
+import 'package:flutter/material.dart';
+
+class FeaturedListViewItem extends StatelessWidget {
+  const FeaturedListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 2.7 / 4,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: Colors.red,
+          image: const DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage(AssetsData.testImage),
+          ),
+        ),
+      ),
+    );
+  }
+}
