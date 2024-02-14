dbcc63b00a2bb0398f072dc39da0d9984dc18187
Khaled Khalifa <96270919+khaledkhalifa74@users.noreply.github.com>
2024-02-14 18:53:26 +0200

    add custom list view item

---
+++ lib/features/Home/presentation/views/widgets/custom_list_view_item.dart
@@ -0,0 +1,26 @@
+import 'package:bookly/core/widgets/assets.dart';
+import 'package:flutter/material.dart';
+
+class CustomListViewItem extends StatelessWidget {
+  const CustomListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.3,
+      child: AspectRatio(
+        aspectRatio: 2.7 / 4,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: Colors.red,
+            image: const DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage(AssetsData.testImage),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/Home/presentation/views/widgets/home_view_body.dart
+++ lib/features/Home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,5 @@
-import 'package:bookly/core/widgets/assets.dart';
 import 'package:bookly/features/Home/presentation/views/widgets/custom_app_bar.dart';
+import 'package:bookly/features/Home/presentation/views/widgets/custom_list_view_item.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -10,6 +10,7 @@ class HomeViewBody extends StatelessWidget {
     return const Column(
       children: [
         CustomAppBar(),
+        CustomListViewItem(),
       ],
     );
   }
